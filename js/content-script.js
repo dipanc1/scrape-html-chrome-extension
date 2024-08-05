29fe5d56ef1ebab582d186c2b7a470597429c4f5
dipanc1 <dipanchhabra@gmail.com>
2024-08-05 23:05:20 +0530

    chore: Update manifest.json and index.html for AI CV Pro extension

--- js/content-script.js
+++ js/content-script.js
@@ -1,42 +1,62 @@
-let button = document.getElementById('html-cpy-btn');
+const copyProfilebutton = document.getElementById('profile-html-cpy-btn');
+const text = document.getElementById('html-cpy-txt');
+
+const copyJobDescriptionbutton = document.getElementById('job-description-html-cpy-btn');
+
+const disableButton = (button) => {
+    button.disabled = true;
+    button.style.cursor = 'not-allowed';
+    button.style.backgroundColor = '#d3d3d3';
+    button.style.color = '#000000';
+}
+
+const enableButton = (button) => {
+    button.disabled = false;
+}
 
 const domain = 'https://aicvpro.com';
 // const domain = 'http://localhost:8007';
 const localDomain = 'http://localhost:3000';
 
-const endpoint = '/api/send-resume/get-clean-resume-text';
+const createResumeEndpoint = '/api/send-resume/get-clean-resume-text';
+const sendJobDescriptionEndpoint = '/api/send-job-description/get-clean-job-description-text';
 
-const redirectUri = '/create?tab=copy-paste';
+const redirectCreateResumeUri = '/create?tab=copy-paste';
+const redirectJobDescriptionCopyUri = '/builder?jobDescription=copy-paste';
 
-const redirectUrl = domain + redirectUri;
+const redirectCreateResumeUrl = domain + redirectCreateResumeUri;
+const redirectJobDescriptionCopyUrl = domain + redirectJobDescriptionCopyUri;
 
-const url = domain + endpoint;
+const createResumeApi = domain + createResumeEndpoint;
+const sendJobDescriptionApi = domain + sendJobDescriptionEndpoint;
 
 const getHtml = () => {
     var html = document.getElementsByTagName('html')[0].innerHTML;
     return html;
 }
 
-if (button) {
-    button.addEventListener('click', function () {
+if (copyProfilebutton) {
+    copyProfilebutton.addEventListener('click', function () {
         chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
             if (tabs.length > 0) {
                 const firstTab = tabs[0];
                 if (!firstTab.url) {
-                    document.querySelector('p').innerText = "Please open your LinkedIn profile first";
+                    text.innerText = "Please open your LinkedIn profile first";
                     return;
                 }
-                if (!firstTab.url.includes('linkedin.com')) {
-                    document.querySelector('p').innerText = "Please open your LinkedIn profile first";
+                if (!firstTab.url.includes('linkedin.com/')) {
+                    text.innerText = "Please open your LinkedIn profile first";
                     return;
                 }
+                disableButton(copyProfilebutton);
+                disableButton(copyJobDescriptionbutton);
                 chrome.tabs.sendMessage(firstTab.id ?? -1, { message: 'copy-html-action' }, (resume) => {
                     if (resume) {
                         var xhr = new XMLHttpRequest();
-                        document.querySelector('button').innerText = "Processing...";
-                        document.querySelector('p').innerText = "Copied profile is being processed...";
+                        copyProfilebutton.innerText = "Sending...";
+                        text.innerText = "Copied profile is being processed...";
 
-                        xhr.open('POST', url, true);
+                        xhr.open('POST', createResumeApi, true);
                         xhr.setRequestHeader('Content-Type', 'application/json');
                         xhr.send(JSON.stringify({ resume }));
 
@@ -45,35 +65,35 @@ if (button) {
                                 navigator.clipboard.writeText(
                                     xhr.responseText
                                 ).then(() => {
-                                    document.querySelector('button').innerText = "Copied!";
-                                    chrome.tabs.update({ url: redirectUrl });
+                                    copyProfilebutton.innerText = "Copied!";
+                                    chrome.tabs.update({ url: redirectCreateResumeUrl });
                                     window.close();
                                 }).catch((e) => {
-                                    document.querySelector('button').innerText = "Error!";
+                                    copyProfilebutton.innerText = "Error!";
                                 });
 
-                                document.querySelector('p').innerText = xhr.responseText;
+                                text.innerText = xhr.responseText;
 
                                 setTimeout(() => {
-                                    document.querySelector('button').innerText = "Send Profile to AICVPro";
+                                    copyProfilebutton.innerText = "Send Profile to AICVPro";
                                 }, 4000);
                             }
                         }
 
                         xhr.onerror = function () {
-                            document.querySelector('button').innerText = "Error!";
+                            copyProfilebutton.innerText = "Error!";
                         }
 
                         xhr.onload = function () {
-                            document.querySelector('button').innerText = "Loaded!";
+                            copyProfilebutton.innerText = "Loaded!";
                         }
 
                         xhr.onprogress = function () {
-                            document.querySelector('button').innerText = "Loading...";
+                            copyProfilebutton.innerText = "Loading...";
                         }
 
                         xhr.ontimeout = function () {
-                            document.querySelector('button').innerText = "Timeout!";
+                            copyProfilebutton.innerText = "Timeout!";
                         }
                     }
                 });
@@ -86,11 +106,83 @@ if (button) {
     );
 }
 
+if (copyJobDescriptionbutton) {
+    copyJobDescriptionbutton.addEventListener('click', function () {
+        chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
+            if (tabs.length > 0) {
+                const firstTab = tabs[0];
+
+                if (!firstTab.url) {
+                    text.innerText = "Please open a job description first";
+                    return;
+                }
+
+                if (!firstTab.url.includes(`linkedin.com/jobs/view/`)) {
+                    text.innerText = "Please open a job description first";
+                    return;
+                }
+
+                chrome.tabs.sendMessage(firstTab.id ?? -1, { message: 'copy-html-action' }, (jobDescription) => {
+                    if (jobDescription) {
+                        disableButton(copyProfilebutton);
+                        disableButton(copyJobDescriptionbutton);
+                        var xhr = new XMLHttpRequest();
+                        copyJobDescriptionbutton.innerText = "Sending...";
+                        text.innerText = "Copied job description is being processed...";
+
+                        xhr.open('POST', sendJobDescriptionApi, true);
+                        xhr.setRequestHeader('Content-Type', 'application/json');
+                        xhr.send(JSON.stringify({ jobDescription }));
+
+                        xhr.onreadystatechange = function () {
+                            if (xhr.readyState == 4 && xhr.status == 200) {
+                                navigator.clipboard.writeText(
+                                    xhr.responseText
+                                ).then(() => {
+                                    copyJobDescriptionbutton.innerText = "Copied!";
+                                    text.innerText = xhr.responseText;
+                                    chrome.tabs.update({ url: redirectJobDescriptionCopyUrl });
+                                    window.close();
+                                }).catch((e) => {
+                                    copyJobDescriptionbutton.innerText = "Error!";
+                                });
+
+                                text.innerText = xhr.responseText;
+
+                                setTimeout(() => {
+                                    copyJobDescriptionbutton.innerText = "Send Job Description to AICVPro";
+                                }, 4000);
+                            }
+                        }
+
+                        xhr.onerror = function () {
+                            copyJobDescriptionbutton.innerText = "Error!";
+                        }
+
+                        xhr.onload = function () {
+                            copyJobDescriptionbutton.innerText = "Loaded!";
+                        }
+
+                        xhr.onprogress = function () {
+                            copyJobDescriptionbutton.innerText = "Loading...";
+                        }
+
+                        xhr.ontimeout = function () {
+                            copyJobDescriptionbutton.innerText = "Timeout!";
+                        }
+                    }
+                });
+            } else {
+                alert('Please open a job description first');
+            }
+
+        });
+    }
+    );
+}
+
 chrome.runtime.onMessage.addListener(function (payload, sender, sendResponse) {
     if (payload.message === 'copy-html-action') {
         sendResponse(getHtml());
     }
 });
-
-// chrome.action.setBadgeText({ text: 'Copy' });
-// chrome.action.setBadgeBackgroundColor({ color: '#4688F1' });
